a558dc277fcee1978261c29e89733bb155ca1d4c
Amjad Jibon <amjad.jibon@gmail.com>
2025-05-27 10:25:05 +0800

    implement command-line input and interactive mode

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	if len(os.Args) > 1 {
+		expr := strings.Join(os.Args[1:], " ")
+		fmt.Println(expr)
+		return
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Enter expressions or 'q'/'exit' to quit.")
+	for {
+		fmt.Print("» ")
+		if !scanner.Scan() {
+			break
+		}
+
+		expr := scanner.Text()
+		if strings.TrimSpace(expr) == "" {
+			continue
+		}
+
+		if expr == "q" || expr == "exit" {
+			fmt.Println("Exiting...")
+			break
+		}
+
+		fmt.Println(expr)
+	}
 }
